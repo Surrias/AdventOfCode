9bdde9e4709632e7d4b918c3bc08a698f0d4bd5c
Zoe Little <64883530+Surrias@users.noreply.github.com>
2024-12-04 09:35:58 -0800

    Complete P2 and reformat

--- Day3/aoc3.py
+++ Day3/aoc3.py
@@ -1,12 +1,20 @@
 import re
 
-with open("Day3\Input.txt",'r') as f:
-    input = f.read()
+def main():
+    with open(r"C:\Users\zoe.little\OneDrive - Broadstreet Properties Ltd\Documents\AoC24\Advent3.txt") as f:
+        input = f.read()
+    print(sum_multis(find_multi(strip_disabled(input))))
 
-multi = re.findall("mul\(\d+,\d+\)", input)
+def strip_disabled(text) -> str:
+    return re.sub("don\'t\(\)[\s\S]*?do\(\)", "", text)
 
-sum = 0
-for pair in multi:
-    set = re.findall("\d+", pair)
-    sum += (int(set[0]) * int(set[1]))
-print(sum)
+def find_multi(text) -> list[str]:
+    return re.findall("mul\(\d+,\d+\)", text)
+
+def sum_multis(pairs):
+    sum = 0
+    for pair in pairs:
+        set = re.findall("\d+", pair)
+        sum += (int(set[0]) * int(set[1]))
+    return sum
+main()
